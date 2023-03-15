380890c35ba8703d9f4989c3cc18eb7bf14c286c
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-03-14 18:24:07 -0600

    Add files via upload

---
+++ Programa5/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnCalcularPromedio = New Button()
+        txtPromedioEstudiante1 = New TextBox()
+        txtPromedioEstudiante2 = New TextBox()
+        txtPromedioEstudiante3 = New TextBox()
+        txtPromedioEstudiante4 = New TextBox()
+        txtPromedioEstudiante5 = New TextBox()
+        txtPromedioClase = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnCalcularPromedio
+        ' 
+        btnCalcularPromedio.Location = New Point(12, 409)
+        btnCalcularPromedio.Name = "btnCalcularPromedio"
+        btnCalcularPromedio.Size = New Size(199, 29)
+        btnCalcularPromedio.TabIndex = 0
+        btnCalcularPromedio.Text = "Calcular Promedio"
+        btnCalcularPromedio.UseVisualStyleBackColor = True
+        ' 
+        ' txtPromedioEstudiante1
+        ' 
+        txtPromedioEstudiante1.Location = New Point(314, 33)
+        txtPromedioEstudiante1.Name = "txtPromedioEstudiante1"
+        txtPromedioEstudiante1.Size = New Size(125, 27)
+        txtPromedioEstudiante1.TabIndex = 1
+        ' 
+        ' txtPromedioEstudiante2
+        ' 
+        txtPromedioEstudiante2.Location = New Point(314, 81)
+        txtPromedioEstudiante2.Name = "txtPromedioEstudiante2"
+        txtPromedioEstudiante2.Size = New Size(125, 27)
+        txtPromedioEstudiante2.TabIndex = 2
+        ' 
+        ' txtPromedioEstudiante3
+        ' 
+        txtPromedioEstudiante3.Location = New Point(314, 114)
+        txtPromedioEstudiante3.Name = "txtPromedioEstudiante3"
+        txtPromedioEstudiante3.Size = New Size(125, 27)
+        txtPromedioEstudiante3.TabIndex = 3
+        ' 
+        ' txtPromedioEstudiante4
+        ' 
+        txtPromedioEstudiante4.Location = New Point(314, 162)
+        txtPromedioEstudiante4.Name = "txtPromedioEstudiante4"
+        txtPromedioEstudiante4.Size = New Size(125, 27)
+        txtPromedioEstudiante4.TabIndex = 4
+        ' 
+        ' txtPromedioEstudiante5
+        ' 
+        txtPromedioEstudiante5.Location = New Point(314, 206)
+        txtPromedioEstudiante5.Name = "txtPromedioEstudiante5"
+        txtPromedioEstudiante5.Size = New Size(125, 27)
+        txtPromedioEstudiante5.TabIndex = 5
+        ' 
+        ' txtPromedioClase
+        ' 
+        txtPromedioClase.Location = New Point(607, 360)
+        txtPromedioClase.Name = "txtPromedioClase"
+        txtPromedioClase.Size = New Size(125, 27)
+        txtPromedioClase.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtPromedioClase)
+        Controls.Add(txtPromedioEstudiante5)
+        Controls.Add(txtPromedioEstudiante4)
+        Controls.Add(txtPromedioEstudiante3)
+        Controls.Add(txtPromedioEstudiante2)
+        Controls.Add(txtPromedioEstudiante1)
+        Controls.Add(btnCalcularPromedio)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnCalcularPromedio As Button
+    Friend WithEvents txtPromedioEstudiante1 As TextBox
+    Friend WithEvents txtPromedioEstudiante2 As TextBox
+    Friend WithEvents txtPromedioEstudiante3 As TextBox
+    Friend WithEvents txtPromedioEstudiante4 As TextBox
+    Friend WithEvents txtPromedioEstudiante5 As TextBox
+    Friend WithEvents txtPromedioClase As TextBox
+End Class
---
+++ Programa5/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub btnCalcularPromedio_Click(sender As Object, e As EventArgs)
+        ' Definir el arreglo de calificaciones de los estudiantes
+        Dim calificaciones(,) As Double = {{7.5, 8.2}, {6.9, 7.8}, {8.1, 7.2}, {9.5, 9.0}, {7.8, 8.3}}
+
+        ' Calcular el promedio de calificaciones de cada estudiante
+        Dim promedioEstudiantes(4) As Double
+        For i As Integer = 0 To 4
+            Dim suma As Double = 0
+            For j As Integer = 0 To 1
+                suma += calificaciones(i, j)
+            Next
+            promedioEstudiantes(i) = suma / 2
+        Next
+
+        ' Calcular el promedio de calificaciones de toda la clase
+        Dim sumaTotal As Double = 0
+        For i As Integer = 0 To 4
+            sumaTotal += promedioEstudiantes(i)
+        Next
+        Dim promedioClase As Double = sumaTotal / 5
+
+        ' Mostrar los resultados en los controles de la interfaz
+        txtPromedioEstudiante1.Text = promedioEstudiantes(0).ToString("F2")
+        txtPromedioEstudiante2.Text = promedioEstudiantes(1).ToString("F2")
+        txtPromedioEstudiante3.Text = promedioEstudiantes(2).ToString("F2")
+        txtPromedioEstudiante4.Text = promedioEstudiantes(3).ToString("F2")
+        txtPromedioEstudiante5.Text = promedioEstudiantes(4).ToString("F2")
+        txtPromedioClase.Text = promedioClase.ToString("F2")
+    End Sub
+End Class
+
